4c20c307591aade1c927e1e0fd60b76ea51c0e92
arimariojesus <arimario.jesus@hotmail.com>
2022-11-19 22:39:10 -0300

    feat: integrates order modal to orders board

--- src/components/OrdersBoard/index.tsx
+++ src/components/OrdersBoard/index.tsx
@@ -1,6 +1,9 @@
-import * as S from './styles';
+import { useState } from 'react';
 
+import { OrderModal } from '@/components/OrderModal';
 import { Order } from '@/types/Order';
+
+import * as S from './styles';
 
 interface OrdersBoardProps {
   icon: string;
@@ -11,8 +14,25 @@ interface OrdersBoardProps {
 export const OrdersBoard = (props: OrdersBoardProps) => {
   const { icon, title, orders } = props;
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+
+  const handleOpenModal = (order: Order) => {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+    setSelectedOrder(null);
+  };
+
   return (
     <S.Board>
+      {!!selectedOrder && (
+        <OrderModal isOpen={isModalVisible} onClose={handleCloseModal} order={selectedOrder} />
+      )}
+
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
@@ -22,7 +42,7 @@ export const OrdersBoard = (props: OrdersBoardProps) => {
       {orders.length > 0 && (
         <S.OrdersContainer>
           {orders.map(order => (
-            <S.OrdersButton type="button" key={order._id}>
+            <S.OrdersButton type="button" key={order._id} onClick={() => handleOpenModal(order)}>
               <strong>Mesa {order.table}</strong>
               <span>
                 {order.products.length} {order.products.length === 1 ? 'item' : 'itens'}
