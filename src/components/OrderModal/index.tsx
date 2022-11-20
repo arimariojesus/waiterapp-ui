2e77dace81d5066f68946ed627c1920ce082d227
arimariojesus <arimario.jesus@hotmail.com>
2022-11-19 22:38:12 -0300

    feat: add OrderModal component

---
+++ src/components/OrderModal/index.tsx
@@ -0,0 +1,93 @@
+import closeIcon from '@/assets/images/close-icon.svg';
+import { Portal } from '@/components/Portal';
+import { ORDER_STATUS } from '@/constants/order-status';
+import { Order } from '@/types/Order';
+import { formatCurrency } from '@/utils/format-currency';
+
+import * as S from './styles';
+
+function getTotal(products: Order['products']) {
+  return products.reduce((total, product) => {
+    const {
+      product: { price },
+      quantity,
+    } = product;
+    return total + price * quantity;
+  }, 0);
+}
+
+interface OrderModalProps {
+  isOpen: boolean;
+  onClose(): void;
+  order: Order;
+}
+
+export const OrderModal = (props: OrderModalProps) => {
+  const { isOpen, onClose, order } = props;
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <Portal>
+      <S.Overlay onClick={onClose}>
+        <S.ModalBody onClick={e => e.stopPropagation()}>
+          <header>
+            <strong>Mesa {order.table}</strong>
+
+            <button type="button" onClick={onClose}>
+              <img src={closeIcon} alt="Fechar" />
+            </button>
+          </header>
+
+          <div className="status-container">
+            <small>Status do Pedido</small>
+            <div>
+              <span>{ORDER_STATUS[order.status].icon}</span>
+              <strong>{ORDER_STATUS[order.status].title}</strong>
+            </div>
+          </div>
+
+          <S.OrderDetails>
+            <strong>Itens</strong>
+
+            <S.OrderItemsContainer>
+              {order.products.map(({ _id, product, quantity }) => (
+                <S.OrderItem key={_id}>
+                  <img
+                    src={`http://localhost:3333/uploads/${product.imagePath}`}
+                    alt={product.name}
+                    width="46"
+                    height="40"
+                  />
+                  <span className="quantity">{quantity}x</span>
+
+                  <div className="product-details">
+                    <strong>{product.name}</strong>
+                    <span>{formatCurrency(product.price)}</span>
+                  </div>
+                </S.OrderItem>
+              ))}
+            </S.OrderItemsContainer>
+
+            <S.Total>
+              <span>Total</span>
+              <strong>{formatCurrency(getTotal(order.products))}</strong>
+            </S.Total>
+          </S.OrderDetails>
+
+          <S.Buttons>
+            <S.ActionButton variant="primary">
+              <span>👩🏾‍🍳</span>
+              <strong>Iniciar produção</strong>
+            </S.ActionButton>
+            <S.ActionButton variant="secondary">
+              <strong>Cancelar pedido</strong>
+            </S.ActionButton>
+          </S.Buttons>
+        </S.ModalBody>
+      </S.Overlay>
+    </Portal>
+  );
+};
---
+++ src/components/OrderModal/styles.ts
@@ -0,0 +1,158 @@
+import styled, { css } from 'styled-components';
+
+export const Overlay = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+
+  background: rgba(0, 0, 0, 0.8);
+  backdrop-filter: blur(4.5px);
+`;
+
+export const ModalBody = styled.div`
+  background: #fff;
+  width: 480px;
+  border-radius: 8px;
+  padding: 32px;
+
+  > * + * {
+    margin-top: 32px;
+  }
+
+  & > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    strong {
+      font-size: 24px;
+    }
+
+    button {
+      line-height: 0;
+      background: transparent;
+      border: 0;
+    }
+  }
+
+  .status-container {
+    & > small {
+      font-size: 14px;
+      opacity: 0.8;
+    }
+
+    & > div {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      margin-top: 8px;
+    }
+  }
+`;
+
+export const OrderDetails = styled.div`
+  & > strong {
+    font-weight: 500;
+    font-size: 14px;
+    opacity: 0.8;
+  }
+`;
+
+export const OrderItemsContainer = styled.div`
+  margin-top: 16px;
+`;
+
+export const OrderItem = styled.div`
+  display: flex;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  & > img {
+    object-fit: cover;
+    border-radius: 6px;
+  }
+
+  .quantity {
+    font-size: 14px;
+    color: #664;
+    display: block;
+    min-width: 20px;
+    margin-left: 12px;
+  }
+
+  .product-details {
+    margin-left: 4px;
+
+    strong {
+      display: block;
+      margin-bottom: 4px;
+    }
+
+    span {
+      font-size: 14px;
+      color: #664;
+    }
+  }
+`;
+
+export const Total = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 24px;
+
+  span {
+    font-weight: 500;
+    font-size: 14px;
+    opacity: 0.8;
+  }
+`;
+
+export const Buttons = styled.footer`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const ActionButton = styled.button<{ variant?: 'primary' | 'secondary' }>`
+  ${({ variant = 'primary' }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+
+    border-radius: 48px;
+    padding: 12px 24px;
+
+    transition: 0.16s;
+
+    ${variant === 'primary'
+      ? css`
+          background: #333;
+          border: 0;
+          color: #fff;
+
+          &:hover {
+            opacity: 0.95;
+          }
+        `
+      : css`
+          background: transparent;
+          border: 1px solid #d73035;
+          color: #d73035;
+
+          &:hover {
+            background: #d7303511;
+          }
+        `}
+  `}
+`;
