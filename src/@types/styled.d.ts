3e5f9e57fb9c304ee1e012624165ddf560152230
arimariojesus <arimario.jesus@hotmail.com>
2022-12-06 23:01:41 -0300

    chore: move @types to inside src folder

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,10 +1,12 @@
 import 'styled-components';
 
-import theme from '../src/styles/theme';
+import theme from '../styles/theme';
 
 export type ThemeMode = 'light' | 'dark';
 export type Theme = typeof theme;
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends Omit<Theme, 'colors'> {}
+  export interface DefaultTheme extends Omit<Theme, 'colors'> {
+    mode: ThemeMode;
+  }
 }
