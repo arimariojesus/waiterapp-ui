fcf227f990d99e2317b31e26c3f16715f37f8b4c
arimariojesus <arimario.jesus@hotmail.com>
2022-12-06 23:04:53 -0300

    feat: implement app theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import { ThemeProvider } from 'styled-components';
-
 import { Header } from '@/components/Header';
 import { Orders } from '@/components/Orders';
-import { OrdersProvider } from '@/contexts';
-import { GlobalStyles } from '@/styles/GlobalStyles';
-import theme from '@/styles/theme';
+import { OrdersProvider, AppThemeProvider } from '@/contexts';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <AppThemeProvider>
       <OrdersProvider>
         <Header />
         <Orders />
-        <GlobalStyles />
       </OrdersProvider>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 }
 
